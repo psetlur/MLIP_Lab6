62f07cb5b491158800045560ca58c941b71d3c4d
Pranav Setlur <psetlur@gmail.com>
2025-02-20 20:55:02 -0500

    Finished to dos in test_utility

--- test_utility.py
+++ test_utility.py
@@ -36,6 +36,7 @@ def feature_target_sample(housing_data_sample):
     return (feature_df, target_series)
 
 def test_data_split(feature_target_sample):
-    return_tuple = data_split(*feature_target_sample)
+    X_train, X_test, y_train, y_test = data_split(*feature_target_sample)
     # TODO test if the length of return_tuple is 4
-    raise NotImplemented
+    assert len((X_train, X_test, y_train, y_test)) == 4
+    
